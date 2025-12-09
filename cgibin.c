3957eac9ef4292e1b43e0c9081c0e89bbdb8787a
Ross Patterson <Ross.Patterson@GMail.com>
2025-12-09 10:13:12 -0500

    HTTP /cgi-bin/tasks/syslog not returning some of syslog. (#793)
    * HTTP /cgi-bin/tasks/syslog not returning some of syslog.
    * Make cgibin_api_v1_syslog() follow the circular buffer too.

--- cgibin.c
+++ cgibin.c
@@ -318,7 +318,7 @@ int     msgcount = 22;
     // Now read the logfile starting at that index. The return
     // value is the total #of bytes of messages data there is.
 
-    if ((num_bytes = log_read( &logbuf_ptr, &logbuf_idx, LOG_NOBLOCK )) > 0)
+    while ((num_bytes = log_read( &logbuf_ptr, &logbuf_idx, LOG_NOBLOCK )) > 0)
     {
         // Copy the message data to a work buffer for processing.
         // This is to allow for the possibility, however remote,
@@ -1638,7 +1638,7 @@ void cgibin_api_v1_syslog(WEBBLK *webblk)
 
     logbuf_idx = msgcount ? log_line( msgcount ) : -1;
 
-    if ((num_bytes = log_read( &logbuf_ptr, &logbuf_idx, LOG_NOBLOCK )) > 0)
+    while ((num_bytes = log_read( &logbuf_ptr, &logbuf_idx, LOG_NOBLOCK )) > 0)
     {
         char   *wrk_bufptr      = malloc( num_bytes );
         char   *sav_wrk         = NULL;
