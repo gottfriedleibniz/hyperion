ceccea237e0887256ae3e0de1a850ecd2d057882
Gottfried Leibniz <37632412+gottfriedleibniz@users.noreply.github.com>
2026-08-16 21:45:54 -0300

    Cont... Testing: Update internals for AArch64

--- hstdinc.h
+++ hstdinc.h
@@ -128,12 +128,12 @@
   #define _GCC_SSE2_
 #endif
 
-#if defined( _M_ARM64 ) || defined( _M_ARM64EC )
+#if defined( __aarch64__ )
+  #include <arm_neon.h>
+#elif defined( _M_ARM64 ) || defined( _M_ARM64EC )
  /* Use the Microsoft-specific header because older VC runtimes fail to
   * redirect arm_neon.h to arm64_neon.h when targeting ARM64EC. */
   #include <arm64_neon.h>
-#elif defined( __aarch64__ )
-  #include <arm_neon.h>
 #endif
 
 #include <stddef.h>             // (ptrdiff_t, size_t, offsetof, etc)
