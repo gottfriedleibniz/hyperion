2c4373cf99cd60615977cde2d85644ef5f938157
Gottfried Leibniz <37632412+gottfriedleibniz@users.noreply.github.com>
2026-08-27 17:20:05 -0300

    msgenu.h: check _MSVC_TRADITIONAL
    Enables compilation with /Zc:preprocessor (or /std:c11 which
    automatically enables the new conforming preprocessor).

--- msgenu.h
+++ msgenu.h
@@ -216,7 +216,7 @@ LOGM_DLL_IMPORT int  panel_command_capture( char* cmd, char** resp, bool quiet )
 #define MSG( id, sev, ... )     #id "%s " id "\n", sev,           ## __VA_ARGS__
 #define MSG_C( id, sev, ... )   #id "%s " id "",   sev,           ## __VA_ARGS__
 
-#if defined( _MSVC_ ) && !defined(__clang__) // MS Windows
+#if defined( _MSVC_ ) && !defined(__clang__) && (!defined( _MSVC_TRADITIONAL ) || _MSVC_TRADITIONAL) // MS Windows
   #define EXTGUIMSG( ... )      send2gui(                         ## __VA_ARGS__ )
 #else // fucking linux
   #define EXTGUIMSG( ... )      send2gui(                            __VA_ARGS__ )
